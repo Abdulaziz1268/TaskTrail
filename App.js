3499d45231f3558a3d23df50906243e9a278e627
Abdulaziz1268 <abdumh1268@gmail.com>
2025-08-20 01:20:04 +0300

    Day 1: project setup

---
+++ App.js
@@ -0,0 +1,32 @@
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { View, Text, StyleSheet, Button } from "react-native"
+
+const stack = createNativeStackNavigator()
+
+function Login({ navigation }) {
+  return (
+    <View>
+      <Text>Login Screen</Text>
+      <Button title="home" onPress={() => navigation.navigate("tasks")} />
+    </View>
+  )
+}
+function TaskListScreen() {
+  return (
+    <View>
+      <Text>Tasks list screen</Text>
+    </View>
+  )
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <stack.Navigator initialRouteName="login">
+        <stack.Screen name="login" component={<Login />} />
+        <stack.Screen name="tasks" component={<TaskListScreen />} />
+      </stack.Navigator>
+    </NavigationContainer>
+  )
+}
