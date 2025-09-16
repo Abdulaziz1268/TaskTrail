8e0f5d34919e72be1c0a055ac7aa2447c8b0157d
Abdulaziz1268 <abdumh1268@gmail.com>
2025-09-17 00:00:23 +0300

    refactor App component structure and add necessary context providers; update dependencies for video support

--- App.js
+++ App.js
@@ -2,20 +2,20 @@ import { NavigationContainer } from "@react-navigation/native"
 import AuthRoutes from "./src/Routes/AuthRoutes"
 import Toast from "react-native-toast-message"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
+import { AuthProvider } from "./src/Context/Auth"
+import { UserProvider } from "./src/Context/User"
 
 export default function App() {
   return (
     <GestureHandlerRootView>
-      <AppRoutes />
-      <Toast position="bottom" />
+      <AuthProvider>
+        <UserProvider>
+          <NavigationContainer>
+            <AuthRoutes />
+            <Toast position="bottom" />
+          </NavigationContainer>
+        </UserProvider>
+      </AuthProvider>
     </GestureHandlerRootView>
-  )
-}
-
-function AppRoutes() {
-  return (
-    <NavigationContainer>
-      <AuthRoutes />
-    </NavigationContainer>
   )
 }
